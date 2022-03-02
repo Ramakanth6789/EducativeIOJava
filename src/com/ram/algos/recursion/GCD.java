c53dc2a4e633fba52ddefb66f7366d4e29729ef4
ramakanth <ramakanth6789@gmail.com>
2022-03-02 10:49:52 +0530

    recursion

---
+++ src/com/ram/algos/recursion/GCD.java
@@ -0,0 +1,20 @@
+package com.ram.algos.recursion;
+
+public class GCD {
+    public static void main(String[] args) {
+        System.out.println(gcd(36,18));
+    }
+
+    public static int gcd(int num1, int num2) {
+        if (num1 == num2) {
+            return num1;
+        }
+        if (num1 > num2) {
+            return gcd(num1 - num2, num2);
+        } else {
+            return gcd(num1, num2 - num1);
+        }
+
+    }
+
+}
---
+++ src/com/ram/algos/recursion/PrimeNumber.java
@@ -0,0 +1,20 @@
+package com.ram.algos.recursion;
+
+public class PrimeNumber {
+    public static void main(String[] args) {
+        int num = 98 ;
+        System.out.println(isPrime(num,num/2));
+    }
+
+    public static boolean isPrime(int num, int i) {
+        if (i == 1) {
+            return true;
+        } else if (num < 2) {
+            return false;
+        } else if (num % i == 0) {
+            return false;
+        } else{
+            return isPrime(num, i - 1);
+        }
+    }
+}
